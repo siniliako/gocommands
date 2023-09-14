ffecdd63ef3a19a1519b9df1484d38ed27093a2a
Daniel Bartolome <DBartolome@tsys.com>
2023-09-13 22:29:10 -0500

    Base of basic printing functions

---
+++ basicprintingfunctions/13FormatingHexadecimalPointerValue.go
@@ -0,0 +1,12 @@
+// It's needed to define package main and func main to create an executable
+package main
+
+import "fmt"
+
+func hello() string {
+	return "Hello, world"
+}
+
+func main() {
+	fmt.Println(hello())
+}
